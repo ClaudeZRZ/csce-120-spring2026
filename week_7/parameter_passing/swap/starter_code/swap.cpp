41d6350f26fd2fbe9acb0e1334239e1772b13723
philip ritchey <pcr@tamu.edu>
2026-02-19 09:35:52 -0600

    add parameter passing examples

---
+++ week_7/parameter_passing/swap/starter_code/swap.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+/**
+ * swaps the values of `a` and `b`
+ * @param a
+ * @param b
+ */
+void my_swap(int a, int b) {
+    // TODO(student): update this function so the swap persists outside the function
+}
+
+int main() {
+    int x = 11;
+    int y = 7;
+
+    std::cout << "before swap: " << std::endl;
+    std::cout << "x: " << x << std::endl;
+    std::cout << "y: " << y << std::endl;
+
+    my_swap(x, y);
+
+    std::cout << "after swap: " << std::endl;
+    std::cout << "x: " << x << std::endl;
+    std::cout << "y: " << y << std::endl;
+
+    return 0;
+}
