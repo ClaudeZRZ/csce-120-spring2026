db2875ed60b0f6916843b6e14daeb0ad1bdb25ac
philip ritchey <pcr@tamu.edu>
2026-03-04 18:12:31 -0600

    use vector<Die>, add overloads for adding Die, Dice, and (number, sides)

--- week_8/dice/class/dice_cup.h
+++ week_8/dice/class/dice_cup.h
@@ -1,33 +1,43 @@
 #pragma once
 
 #include <vector>
+#include "die.h"
+#include "dice.h"
 
 using std::vector;
 
 class DiceCup {
-    vector<Dice> dice;
+    vector<Die> dice;
 
  public:
     DiceCup() : dice{} {}
-    unsigned count() const {
-        unsigned v = 0;
-        for (const Dice& d : dice) {
-            v += d.count();
-        }
-        return v;
-    }
+
+    unsigned count() const { return dice.size(); }
+
     unsigned value() const {
         unsigned v = 0;
-        for (const Dice& d : dice) {
+        for (const Die& d : dice) {
             v += d.value();
         }
         return v;
     }
-    void add(Dice d) {
+
+    void add(Die d) {
         dice.push_back(d);
     }
+
+    void add(Dice d) {
+        for (unsigned i = 0; i < d.count(); i++) {
+            dice.push_back(Die(d.sides()));
+        }
+    }
+
+    void add(int number, int sides) {
+        this->add(Dice(number, sides));
+    }
+
     void roll() {
-        for (Dice& d : dice) {
+        for (Die& d : dice) {
             d.roll();
         }
     }
